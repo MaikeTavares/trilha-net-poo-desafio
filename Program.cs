e04b07ab7706b7478fd4e965c413330aae05a691
Maike Tavares <maiketv_bn@hotmail.com>
2023-12-26 16:07:59 -0300

    first-commit-realizado

--- Program.cs
+++ Program.cs
@@ -1,3 +1,19 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Teste do nokia");
+Smartphone nokia = new Nokia(numero: "95672-2222", modelo: "NokiaL4", imei: "000000000", memoria: 128);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Android");
+
+
+Console.WriteLine("\n");
+
+
+Console.WriteLine("Teste do Iphone");
+Smartphone iphone = new Iphone(numero: "94567-2222", modelo: "14 Pro Max", imei: "111111111", memoria: 256);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("IOS");
+
